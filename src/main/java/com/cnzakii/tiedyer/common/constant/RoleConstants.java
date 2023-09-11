eb0602960da75752e5c4d77698466e93385820be
zaki <cnzakii@gmail.com>
2023-09-11 19:50:58 +0800

    chore: 添加常用工具类和配置

---
+++ src/main/java/com/cnzakii/tiedyer/common/constant/RoleConstants.java
@@ -0,0 +1,11 @@
+package com.cnzakii.tiedyer.common.constant;
+
+/**
+ * 角色常量
+ *
+ * @author Zaki
+ * @since 2023-09-04
+ **/
+public interface RoleConstants {
+    String ROLE_USER = "R_USER";
+}
---
+++ src/main/java/com/cnzakii/tiedyer/common/constant/SaTokenConstants.java
@@ -0,0 +1,35 @@
+package com.cnzakii.tiedyer.common.constant;
+
+/**
+ * <p>
+ * Sa-Token 常量
+ * <p>
+ *
+ * @author Zaki
+ * @version 1.0
+ * @since 2023-03-18
+ **/
+public interface SaTokenConstants {
+
+
+    /**
+     * 二级认证-更新用户密码
+     */
+    String SAFE_UPDATE_PASSWORD = "update:user:password";
+
+    /**
+     * 二级认证-更新用户邮箱
+     */
+    String SAFE_UPDATE_EMAIL = "update:user:password";
+
+    /**
+     * 二级认证-删除用户
+     */
+    String SAFE_DELETE_USER = "delete:user";
+
+    /**
+     * 二级认证过期时间-秒
+     */
+    Long SAFE_TIME = 300L;
+
+}
