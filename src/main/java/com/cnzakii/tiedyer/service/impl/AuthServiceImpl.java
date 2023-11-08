6fe78f99ecee33884c29bfa022fc60e03eb95e47
zaki <cnzakii@gmail.com>
2023-11-08 20:53:26 +0800

    fix: 修复一些bug

--- src/main/java/com/cnzakii/tiedyer/service/impl/AuthServiceImpl.java
+++ src/main/java/com/cnzakii/tiedyer/service/impl/AuthServiceImpl.java
@@ -1,13 +1,17 @@
 package com.cnzakii.tiedyer.service.impl;
 
 import com.cnzakii.tiedyer.entity.User;
+import com.cnzakii.tiedyer.exception.BusinessException;
 import com.cnzakii.tiedyer.model.dto.user.AuthUserDTO;
 import com.cnzakii.tiedyer.service.AuthService;
 import com.cnzakii.tiedyer.service.UserService;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.annotation.Resource;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -15,6 +19,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Objects;
+
+import static com.cnzakii.tiedyer.common.http.ResponseStatus.*;
 
 
 /**
@@ -67,33 +74,33 @@ public class AuthServiceImpl implements AuthService {
                 .toUri();
 
 
-//        // 发送请求
-//        ResponseEntity<String> result = this.restTemplate.getForEntity(uri, String.class);
-//
-//        // 解析响应内容
-//        String response = result.getBody();
-//        JsonNode jsonNode = new ObjectMapper().readTree(response);
-//        // 尝试获取openId
-//        JsonNode node = jsonNode.get("openid");
-//        if (node == null) {
-//            log.error("小程序登录失败: {}", response);
-//            // 获取对应状态码
-//            String errCode = jsonNode.get("errcode").asText();
-//
-//            if (Objects.equals(errCode, "40029")) {
-//                // js_code 无效
-//                throw new BusinessException(NO_AUTHENTICATION, "js_code无效");
-//            } else if (Objects.equals(errCode, "40226")) {
-//                // 账号高危
-//                throw new BusinessException(NO_AUTHENTICATION, "账号高危");
-//            } else {
-//                throw new BusinessException(SERVER_ERROR);
-//            }
-//        }
-//
-//        String openId = node.asText();
+        // 发送请求
+        ResponseEntity<String> result = this.restTemplate.getForEntity(uri, String.class);
+        log.info(result.getBody());
 
-        String openId = "2";
+        // 解析响应内容
+        String response = result.getBody();
+        JsonNode jsonNode = new ObjectMapper().readTree(response);
+        // 尝试获取openId
+        JsonNode node = jsonNode.get("openid");
+        if (node == null) {
+            log.error("小程序登录失败: {}", response);
+            // 获取对应状态码
+            String errCode = jsonNode.get("errcode").asText();
+
+            if (Objects.equals(errCode, "40029")) {
+                // js_code 无效
+                throw new BusinessException(NO_AUTHENTICATION, "js_code无效");
+            } else if (Objects.equals(errCode, "40226")) {
+                // 账号高危
+                throw new BusinessException(NO_AUTHENTICATION, "账号高危");
+            } else {
+                throw new BusinessException(SERVER_ERROR);
+            }
+        }
+
+        String openId = node.asText();
+
 
         // 尝试从数据库中获取user信息
         User user = userService.getUserInfoByOpenId(openId);
