3ca353238cc52358b0399216e7c242baee05f276
zaki <cnzakii@gmail.com>
2023-11-22 20:25:23 +0800

    perf: 优化全局异常处理逻辑

--- src/main/java/com/cnzakii/tiedyer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cnzakii/tiedyer/exception/GlobalExceptionHandler.java
@@ -1,13 +1,17 @@
 package com.cnzakii.tiedyer.exception;
 
 import com.cnzakii.tiedyer.common.http.ResponseResult;
+import com.cnzakii.tiedyer.common.http.ResponseStatus;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -42,6 +46,38 @@ public class GlobalExceptionHandler {
     }
 
     /**
+     * 处理请求参数异常<br/>
+     * 例如：请求参数缺失，请求体缺失
+     *
+     * @param e exception
+     * @return ResponseResult
+     */
+    @ResponseBody
+    @ExceptionHandler(value = {
+            MissingServletRequestParameterException.class, // 请求参数缺失
+            HttpMessageNotReadableException.class // 请求体缺失
+    })
+    public ResponseResult<BusinessException> handleRequestException(Exception e, HttpServletRequest request) {
+        String errorMsg;
+
+        if (e instanceof MissingServletRequestParameterException parameterException) {
+            String parameterName = parameterException.getParameterName();
+            errorMsg = "Required request parameter " + parameterName + " is missing";
+        } else if(e instanceof HttpMessageNotReadableException){
+            errorMsg =  "Required request body is missing";
+        }else {
+            errorMsg = "Illegal request";
+        }
+
+
+        log.error("[{}] {} --> ResponseCode：{},Exception: {}",
+                request.getMethod(), request.getRequestURI(), REQUEST_ERROR.getCode(), e.getLocalizedMessage());
+
+        return ResponseResult.base(REQUEST_ERROR, errorMsg);
+    }
+
+
+    /**
      * 处理参数验证异常
      *
      * @param e exception
@@ -52,77 +88,86 @@ public class GlobalExceptionHandler {
             BindException.class,
             ValidationException.class
     })
-    public ResponseResult<String> handleParameterVerificationException(Exception e) {
-        List<String> exceptionMsg = new ArrayList<>();
-        log.error("ResponseCode：400,Exception: {}", e.getMessage());
+    public ResponseResult<String> handleParameterVerificationException(Exception e, HttpServletRequest request) {
+        List<String> errMsgList = new ArrayList<>();
 
         if (e instanceof BindException bindException) {
             bindException.getBindingResult().getAllErrors()
-                    .forEach(a -> exceptionMsg.add(a.getDefaultMessage()));
+                    .forEach(a -> errMsgList.add(a.getDefaultMessage()));
         } else if (e instanceof ConstraintViolationException violationException) {
             violationException.getConstraintViolations()
                     .stream()
                     .map(ConstraintViolation::getMessage)
-                    .forEach(exceptionMsg::add);
+                    .forEach(errMsgList::add);
         } else {
-            exceptionMsg.add("invalid parameter");
+            errMsgList.add("invalid parameter");
         }
 
-        return ResponseResult.base(REQUEST_ERROR, String.join(",", exceptionMsg));
+        String errMsg = String.join(",", errMsgList);
+
+        log.error("[{}] {} --> ResponseCode：{},Exception: {}",
+                request.getMethod(), request.getRequestURI(), REQUEST_ERROR.getCode(), errMsg);
+
+        return ResponseResult.base(REQUEST_ERROR, errMsg);
+    }
+
+
+    /**
+     * 处理安全异常
+     *
+     * @param e 安全异常
+     * @return ResponseResult
+     */
+    @ResponseBody
+    @ExceptionHandler(value = {
+            AuthenticationException.class, // 认证异常
+            AccessDeniedException.class    // 权限异常
+    })
+    public ResponseResult<String> handleSecurityException(Exception e, HttpServletRequest request) {
+        ResponseStatus responseStatus;
+
+        if (e instanceof AuthenticationException) {
+            responseStatus = NO_AUTHENTICATION;
+        } else if (e instanceof AccessDeniedException) {
+            responseStatus = NO_AUTHORITIES;
+        } else {
+            responseStatus = FAIL;
+        }
+
+        log.error("[{}] {} --> ResponseCode：{},Exception: {}",
+                request.getMethod(), request.getRequestURI(), responseStatus.getCode(), e.getLocalizedMessage());
+
+        return ResponseResult.base(responseStatus, responseStatus.getDescription());
     }
 
 
     /**
      * 处理业务异常
      *
-     * @param businessException business exception
+     * @param e business exception
      * @return ResponseResult
      */
     @ResponseBody
     @ExceptionHandler(BusinessException.class)
-    public ResponseResult<BusinessException> processBusinessException(BusinessException businessException) {
-        log.error("ResponseCode：{},Exception: {}", businessException.getCode(), businessException.getDescription());
-        return ResponseResult.base(businessException.getCode(), null, businessException.getDescription());
-    }
+    public ResponseResult<BusinessException> handleBusinessException(BusinessException e, HttpServletRequest request) {
 
-    /**
-     * 处理认证异常
-     *
-     * @param authenticationException authenticationException
-     * @return ResponseResult
-     */
-    @ResponseBody
-    @ExceptionHandler(AuthenticationException.class)
-    public ResponseResult<String> processNotLoginException(AuthenticationException authenticationException) {
-        log.error("ResponseCode：{},Exception: {}", NO_AUTHENTICATION.getCode(), authenticationException.getLocalizedMessage());
-        return ResponseResult.base(NO_AUTHENTICATION, NO_AUTHENTICATION.getDescription());
-    }
+        log.error("[{}] {} --> ResponseCode：{},Exception: {}",
+                request.getMethod(), request.getRequestURI(), e.getCode(), e.getDescription());
 
-    /**
-     * 处理权限异常
-     *
-     * @param accessDeniedException accessDeniedException
-     * @return ResponseResult
-     */
-    @ResponseBody
-    @ExceptionHandler(AccessDeniedException.class)
-    public ResponseResult<String> processNotPermissionException(AccessDeniedException accessDeniedException) {
-        log.error("ResponseCode：{},Exception: {}", NO_AUTHORITIES.getCode(), accessDeniedException.getLocalizedMessage());
-        return ResponseResult.base(NO_AUTHORITIES, NO_AUTHORITIES.getDescription());
+        return ResponseResult.base(e.getCode(), null, e.getDescription());
     }
-
 
     /**
      * 处理其他异常
      *
-     * @param exception exception
+     * @param e exception
      * @return ResponseResult
      */
     @ResponseBody
     @ExceptionHandler(Exception.class)
-    public ResponseResult<Exception> processException(Exception exception) {
-
-        log.error("ResponseCode：500,Exception: {}", exception.getLocalizedMessage());
+    public ResponseResult<String> handleOtherException(Exception e, HttpServletRequest request) {
+        log.error("[{}] {} --> ResponseCode：{},Exception: {}",
+                request.getMethod(), request.getRequestURI(), SERVER_ERROR.getCode(), e.getLocalizedMessage());
 
         return ResponseResult.base(SERVER_ERROR, SERVER_ERROR.getDescription());
     }
