a6189e59b05e5e1bc1da60dff004ca195c5fac6e
zaki <cnzakii@gmail.com>
2023-11-27 16:49:10 +0800

    perf: 取消直接存储微信头像路径，而是将当前用户头像保存至本地。

--- src/main/java/com/cnzakii/tiedyer/service/impl/UserServiceImpl.java
+++ src/main/java/com/cnzakii/tiedyer/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.cnzakii.tiedyer.service.impl;
 
 import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -11,12 +12,16 @@ import com.cnzakii.tiedyer.exception.BusinessException;
 import com.cnzakii.tiedyer.mapper.UserMapper;
 import com.cnzakii.tiedyer.service.UserPointsHistoryService;
 import com.cnzakii.tiedyer.service.UserService;
+import com.cnzakii.tiedyer.util.MyFileUtils;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
@@ -31,6 +36,7 @@ import static com.cnzakii.tiedyer.common.constant.RoleConstants.ROLE_USER;
  * @since 2023-10-26
  */
 @Service
+@Slf4j
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
     @Resource
@@ -111,22 +117,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
 
 
     /**
-     * 根据userId更新用户昵称和头像
+     * 根据userId更新用户昵称
      *
-     * @param userId     用户ID
-     * @param nickName   昵称
-     * @param avatarPath 头像路径
+     * @param userId   用户ID
+     * @param nickName 昵称
      */
     @Override
-    public void updateUserInfo(Long userId, String nickName, String avatarPath) {
-        if (StringUtils.isAnyBlank(nickName, avatarPath)) {
-            throw new BusinessException(ResponseStatus.REQUEST_ERROR, "昵称或者头像路径为空");
+    public void updateUserInfo(Long userId, String nickName) {
+        if (StringUtils.isBlank(nickName)) {
+            throw new BusinessException(ResponseStatus.REQUEST_ERROR, "昵称无效");
         }
 
         // 更新user信息
         int i = userMapper.update(null, new LambdaUpdateWrapper<User>()
                 .set(User::getNickName, nickName)
-                .set(User::getAvatarPath, avatarPath)
                 .eq(User::getId, userId)
         );
         if (i == 0) {
@@ -140,15 +144,60 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
     }
 
     /**
-     * 更新用户积分
+     * 更新用户头像
      *
      * @param userId 用户id
-     * @param points 增加的积分数
+     * @param avatar 头像
+     * @return 头像地址
+     */
+    @Override
+    public String updateUserAvatar(Long userId, MultipartFile avatar) {
+        // 根据Id获取当前用户
+        User user = getUserInfoById(userId);
+
+        String oldIconPath = user.getAvatarPath();
+        if (StringUtils.isBlank(oldIconPath)) {
+            // 删除旧的avatar
+            MyFileUtils.deleteFile(oldIconPath);
+        }
+
+        // 上传头像
+        LocalDate localDate = LocalDate.now();
+        String subPath = "/avatar/" + localDate.getYear() + "/" + localDate.getMonthValue() + "/" + localDate.getDayOfMonth() + "/";
+
+        String avatarPath = MyFileUtils.uploadFile(subPath, avatar);
+
+        if (StrUtil.isEmpty(avatarPath)) {
+            throw new BusinessException(ResponseStatus.SERVER_ERROR, "头像保存失败");
+        }
+
+        // 更新数据库
+        int i = userMapper.update(null, new LambdaUpdateWrapper<User>().eq(User::getId, userId).set(User::getAvatarPath, avatarPath));
+        if (i == 0) {
+            log.error("更新用户({})头像失败失败", userId);
+            throw new BusinessException(ResponseStatus.FAIL, "更新失败");
+        }
+
+
+        // 更新Redis缓存
+        String key = USER_INFO + userId;
+        stringRedisTemplate.delete(key);
+        getUserInfoById(userId);
+
+        // 返回子路径
+        return avatarPath;
+    }
+
+    /**
+     * 更新用户积分
+     *
+     * @param userId      用户id
+     * @param points      增加的积分数
      * @param description 描述
      */
     @Transactional
     @Override
-    public void updatePoints(Long userId, int points,String description) {
+    public void updatePoints(Long userId, int points, String description) {
         // 添加积分
         userMapper.updatePoints(userId, points);
         // 添加日志
